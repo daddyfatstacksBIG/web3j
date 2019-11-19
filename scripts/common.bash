90b9c8569449555a09006c6add217c15e864c651
Restyled.io <commits@restyled.io>
2019-11-19 15:34:09 +0000

    Restyled by shellharden

--- scripts/common.bash
+++ scripts/common.bash
@@ -8,8 +8,8 @@ configure_github_user() {
 }
 
 github_clone() {
-    git clone https://antonydenyer:${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/web3j/$1.git
-    cd $1
+    git clone https://antonydenyer:"$GITHUB_PERSONAL_ACCESS_TOKEN@github.com/web3j/$1".git
+    cd "$1"
 }
 
 ensure_version() {
