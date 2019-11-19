f6ba42540077e3c24b379d813cd4361f99bdf851
Restyled.io <commits@restyled.io>
2019-11-19 15:34:09 +0000

    Restyled by shfmt

--- codegen/src/test/resources/solidity/build.sh
+++ codegen/src/test/resources/solidity/build.sh
@@ -13,21 +13,21 @@ duplicate/DuplicateField
 "
 
 for target in "$targets"; do
-    dirName=$(dirname "$target")
-    fileName=$(basename "$target")
+  dirName=$(dirname "$target")
+  fileName=$(basename "$target")
 
-    cd "$dirName"
-    echo "Compiling Solidity files in $dirName:"
-    solc --bin --abi --optimize --overwrite "$fileName".sol -o build/
-    echo "Complete"
+  cd "$dirName"
+  echo "Compiling Solidity files in $dirName:"
+  solc --bin --abi --optimize --overwrite "$fileName".sol -o build/
+  echo "Complete"
 
-    echo "Generating web3j bindings"
-    web3j solidity generate \
-        -b build/"$fileName".bin \
-        -a build/"$fileName".abi \
-        -p org.web3j.generated \
-        -o ../../../../../../integration-tests/src/test/java/ > /dev/null
-    echo "Complete"
+  echo "Generating web3j bindings"
+  web3j solidity generate \
+    -b build/"$fileName".bin \
+    -a build/"$fileName".abi \
+    -p org.web3j.generated \
+    -o ../../../../../../integration-tests/src/test/java/ >/dev/null
+  echo "Complete"
 
-    cd -
+  cd -
 done
