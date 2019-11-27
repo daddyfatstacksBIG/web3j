6f78949d5c68e3697a03c751ef3931138ff4ed13
Restyled.io <commits@restyled.io>
2019-11-27 14:21:15 +0000

    Restyled by clang-format

--- codegen/src/main/java/org/web3j/codegen/unit/gen/ClassProvider.java
+++ codegen/src/main/java/org/web3j/codegen/unit/gen/ClassProvider.java
@@ -1,14 +1,17 @@
 /*
  * Copyright 2019 Web3 Labs Ltd.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.web3j.codegen.unit.gen;
 
@@ -25,50 +28,53 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ClassProvider {
-    private final File pathToJavaFiles;
+  private final File pathToJavaFiles;
 
-    public ClassProvider(final File pathToJavaFiles) {
-        this.pathToJavaFiles = pathToJavaFiles;
-    }
+  public ClassProvider(final File pathToJavaFiles) {
+    this.pathToJavaFiles = pathToJavaFiles;
+  }
 
-    public final List<Class> getClasses() throws IOException {
-        return loadClassesToList(compileClasses());
-    }
+  public final List<Class> getClasses() throws IOException {
+    return loadClassesToList(compileClasses());
+  }
 
-    private CompilerClassLoader compileClasses() throws IOException {
-        URL[] classPathURL = new URL[] {pathToJavaFiles.toURI().toURL()};
-        Path outputDirectory = Files.createTempDirectory("tmp");
-        return new CompilerClassLoader(
-                Objects.requireNonNull(outputDirectory).toFile(), classPathURL);
-    }
+  private CompilerClassLoader compileClasses() throws IOException {
+    URL[] classPathURL = new URL[] {pathToJavaFiles.toURI().toURL()};
+    Path outputDirectory = Files.createTempDirectory("tmp");
+    return new CompilerClassLoader(
+        Objects.requireNonNull(outputDirectory).toFile(), classPathURL);
+  }
 
-    private List<Class> loadClassesToList(final CompilerClassLoader compilerClassLoader)
-            throws IOException {
-        return getFormattedClassPath().stream()
-                .map(
-                        cp -> {
-                            try {
-                                return compilerClassLoader.loadClass(
-                                        cp.replace(File.separator, "."));
-                            } catch (ClassNotFoundException e) {
-                                throw new RuntimeException(e);
-                            }
-                        })
-                .collect(Collectors.toList());
-    }
+  private List<Class>
+  loadClassesToList(final CompilerClassLoader compilerClassLoader)
+      throws IOException {
+    return getFormattedClassPath()
+        .stream()
+        .map(cp -> {
+          try {
+            return compilerClassLoader.loadClass(
+                cp.replace(File.separator, "."));
+          } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+          }
+        })
+        .collect(Collectors.toList());
+  }
 
-    private List<String> getFormattedClassPath() throws IOException {
-        Stream<Path> walk = Files.walk(Paths.get(pathToJavaFiles.toURI()));
-        return getClassPathFromURL(
-                walk.filter(Files::isRegularFile).map(Path::toString).collect(Collectors.toList()));
-    }
+  private List<String> getFormattedClassPath() throws IOException {
+    Stream<Path> walk = Files.walk(Paths.get(pathToJavaFiles.toURI()));
+    return getClassPathFromURL(walk.filter(Files::isRegularFile)
+                                   .map(Path::toString)
+                                   .collect(Collectors.toList()));
+  }
 
-    private List<String> getClassPathFromURL(final List<String> listOfUrl) throws IOException {
-        int length = pathToJavaFiles.getCanonicalPath().length();
-        List<String> formattedClassPath = new ArrayList<>();
-        for (String s : listOfUrl) {
-            formattedClassPath.add(s.substring(length + 1, s.lastIndexOf(".java")));
-        }
-        return formattedClassPath;
+  private List<String> getClassPathFromURL(final List<String> listOfUrl)
+      throws IOException {
+    int length = pathToJavaFiles.getCanonicalPath().length();
+    List<String> formattedClassPath = new ArrayList<>();
+    for (String s : listOfUrl) {
+      formattedClassPath.add(s.substring(length + 1, s.lastIndexOf(".java")));
     }
+    return formattedClassPath;
+  }
 }
